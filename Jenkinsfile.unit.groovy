e6758d89a57e2c210057b3d31221ba9c6d6178dd
Hernan Espinoza <89279451+jhespinoza26@users.noreply.github.com>
2024-06-16 15:51:31 -0500

    Update Jenkinsfile.unit.groovy

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -48,6 +48,14 @@ pipeline {
                 }
             }
         }
+        stage('Print logs') {
+            steps {
+                script {
+                    echo "Trabajo: ${env.JOB_NAME}"
+                    echo "Ejecución número: ${env.BUILD_NUMBER}"
+                }
+            }
+        }
     }
     post {
         always {
@@ -55,7 +63,7 @@ pipeline {
         }
         failure {
             mail(
-                to: 'jairohernan.espinoza245@comunidadunir.net',
+                to: 'lab1@comunidadunir.net',
                 subject: "Failed: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
                 body: "The job ${env.JOB_NAME} #${env.BUILD_NUMBER} has failed. Check the logs for more information."
             )
