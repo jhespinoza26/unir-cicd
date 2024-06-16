8e427acde802c7bd315cada49c95cb375990df00
Jairo Hernan Espinoza <jaiespin@pichincha.com>
2024-06-16 13:27:28 -0500

    add stage for tests e2e

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -2,7 +2,16 @@ pipeline {
     agent {
         label 'docker'
     }
+    triggers {
+        // Se agrega un cron para ejecutar el build cada 5 minutos
+        pollSCM('H/5 * * * *')
+    }
     stages {
+        stage('Source') {
+            steps {
+                git 'https://github.com/jhespinoza26/unir-cicd'
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building stage!'
@@ -12,13 +21,44 @@ pipeline {
         stage('Unit tests') {
             steps {
                 sh 'make test-unit'
-                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('API Tests') {
+            steps {
+                echo 'Running API tests'
+                sh 'make test-api'
+                
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: 'api-tests-results/*.xml'
+                    junit 'api-tests-results/*.xml'
+                }
+            }
+        }
+        stage('E2E Tests') {
+            steps {
+                echo 'Running E2E tests'
+                sh 'make test-e2e'  // Asegúrate de que este comando ejecuta tus pruebas E2E
+            }
+            post {
+                always {
+                    archiveArtifacts artifacts: 'e2e-tests-results/*.xml'
+                    junit 'e2e-tests-results/*.xml'
+                }
             }
         }
     }
     post {
         always {
-            junit 'results/*_result.xml'
+            junit 'results/*.xml'
+        }
+        failure {
+            mail(
+                to: 'jairohernan.espinoza245@comunidadunir.net',
+                subject: "Failed: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
+                body: "The job ${env.JOB_NAME} #${env.BUILD_NUMBER} has failed. Check the logs for more information."
+            )
         }
     }
 }
